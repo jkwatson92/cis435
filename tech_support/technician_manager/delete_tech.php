157ca4af0b4ad0c2c768d54628248a80773e5fbc
jkwatson92 <jkwatson@umich.edu>
2018-11-24 13:55:12 -0500

    products boiiii

--- tech_support/technician_manager/delete_tech.php
+++ tech_support/technician_manager/delete_tech.php
@@ -3,8 +3,6 @@ require_once('../model/database.php');
 
 //Delete the tech from the database
 $tech_id = filter_input(INPUT_POST, 'tech_id',FILTER_VALIDATE_INT);
-// echo "'<script>console.log(\"$tech_id\")</script>'";
-// echo '<script>console.log("DO A THING")</script>';
 if($tech_id!=false){
   $query = 'DELETE FROM technicians WHERE techID =:tech_id';
   $statement=$db->prepare($query);
