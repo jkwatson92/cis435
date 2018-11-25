2f87752744bfeaec3313f0cd3f2b85b90a746eb2
Ashley Baker <ashleybm@umich.edu>
2018-11-25 15:17:45 -0500

    remove debugging tools from 6.4 and 6.5

--- tech_support/product_manager/product_registered.php
+++ tech_support/product_manager/product_registered.php
@@ -1,18 +1,7 @@
 <?php
 
-function debug_to_console( $data ) {
-    $output = $data;
-    if ( is_array( $output ) )
-        $output = implode( ',', $output);
-
-    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-}
-
 $cID=filter_input(INPUT_POST,'custID');
 $productCode=filter_input(INPUT_POST, 'product');
-
-debug_to_console( $cID );
-debug_to_console( $productCode );
 
 if($productCode==null || $cID==null){
   $error="Invalid product data. Check all fields and try again.";
@@ -24,7 +13,6 @@ else{
 
 //Date Information
 $openDate = date('Y-m-d H:i:s');
-debug_to_console( $openDate );
 
 //Add the product to the database
 $query='INSERT INTO registrations (customerID, productCode, registrationDate)
@@ -35,7 +23,6 @@ $statement = $db->prepare($query);
 $statement->bindValue(':customerID',$cID);
 $statement->bindValue(':productCode',$productCode);
 $statement->bindValue(':registrationDate',$openDate);
-debug_to_console($statement->execute());
 $statement->closeCursor();
 }
 
