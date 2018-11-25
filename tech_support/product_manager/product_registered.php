d62cce19e0a9bb20015ff52acf7d30eb2c27e7d3
Ashley Baker <ashleybm@umich.edu>
2018-11-25 00:58:48 -0500

    Add new files, and start product_registered

---
+++ tech_support/product_manager/product_registered.php
@@ -0,0 +1,18 @@
+<?php include '../view/header.php'; ?>
+<!DOCTYPE html>
+<html>
+<!-- the body section -->
+<body>
+  <main>
+    <section>
+      <!-- display a table of technicians -->
+      <h2>Register Product</h2>
+      <div>
+        //TODO: Finish the PHP injection here.
+        Product <?php echo $productCode ?>was registered successfully.
+      </div>
+    </section>
+  </main>
+</body>
+</html>
+<?php include '../view/footer.php'; ?>
